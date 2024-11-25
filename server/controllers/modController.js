69ed3b1be5597dc74ee8781df2397e5fd14314cd
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-11-25 15:53:15 +0400

    mod backend starts

---
+++ server/controllers/modController.js
@@ -0,0 +1,5 @@
+import Mod from "../models/modModel";
+
+const createMod = async (req, res) => {
+  res.status(200).json({ mssg: "create mod application" });
+};
---
+++ server/models/modModel.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const mod = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    dateFrom: {
+      type: Date,
+      required: true,
+    },
+    dateTo: {
+      type: Date,
+      required: true,
+    },
+    area: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    companyName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    textArea: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Mod = mongoose.model("Mod", modSchema);
+
+export default Mod;
