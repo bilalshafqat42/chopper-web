23ebab99fead38632b432c456209d3939fdb527b
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-11-27 12:39:44 +0400

    components updated

--- frontend/src/pages/mod/PendingMod.jsx
+++ frontend/src/pages/mod/PendingMod.jsx
@@ -1,8 +1,95 @@
 import React from 'react'
+import ModSidebar from '../../components/mod/ModSidebar'
+import DataTable from "react-data-table-component"
 
 const PendingMod = () => {
+    // A super simple expandable component.
+    // const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+
+    const columns = [
+        {
+            name: 'Name',
+            selector: row => row.title,
+        },
+        {
+            name: 'Date',
+            selector: row => row.date,
+        },
+        {
+            name: 'Area',
+            selector: row => row.area,
+        },
+        {
+            name: 'Company',
+            selector: row => row.company,
+        },
+    ];
+
+    const data = [
+        {
+            id: 1,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+        {
+            id: 2,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+        {
+            id: 3,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+        {
+            id: 4,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+        {
+            id: 5,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+        {
+            id: 6,
+            title: 'MOD',
+            date: '24-Nov-2024',
+            area: "al quoz 3 Industrial, Dubai, UAE",
+            company: "Choppershoot LLC",
+        },
+
+    ]
     return (
-        <div>PendingMod</div>
+        <>
+            <ModSidebar />
+            <div className='position-absolute top-20 end-0 w-75 h-100'>
+                <div className='container main-section-dashboard rounded px-5 py-5'>
+                    <h1>Dashboard</h1>
+                    <DataTable
+                        columns={columns}
+                        data={data}
+                    // expandableRows
+                    // expandableRowsComponent={ExpandedComponent}
+                    />
+                </div>
+            </div>
+        </>
     )
 }
 
--- frontend/src/pages/mod/ViewMod.jsx
+++ frontend/src/pages/mod/ViewMod.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import ModSidebar from '../../components/mod/ModSidebar'
 
 const ViewMod = () => {
     return (
-        <div>ViewMod</div>
+        <>
+            <ModSidebar />
+            <div className='position-absolute top-20 end-0 w-75 h-100'>
+                <div className='container main-section-dashboard rounded px-5 py-5'>
+                    <h1>Dashboard</h1>
+                    <DataTable
+                        columns={columns}
+                        data={data}
+                    // expandableRows
+                    // expandableRowsComponent={ExpandedComponent}
+                    />
+                </div>
+            </div>
+        </>
     )
 }
 
