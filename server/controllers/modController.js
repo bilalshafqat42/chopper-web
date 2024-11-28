cba115545e391af7c76e74507ec85eea56c74ebf
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-11-28 08:48:57 +0400

    new updates in server

--- server/controllers/modController.js
+++ server/controllers/modController.js
@@ -1,11 +1,66 @@
 import Mod from "../models/modModel.js";
+import mongoose from "mongoose";
 
+// get all mod applications
+
+const getMod = async (req, res) => {
+  const mods = await Mod.find({}).sort({ createdAt: -1 });
+  res.status(200).json(mods);
+};
+
+// get single mod application
+const getMods = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  const mod = await Mod.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  res.status(200).json(mod);
+};
+
+// create a new mod
 const createMod = async (req, res) => {
-  res.status(200).json({ mssg: "create mod application" });
+  const { name, email, dateFrom, dateTo, area, companyName, textArea } =
+    req.body;
+
+  // add to the database
+  try {
+    const mod = await Mod.create({
+      name,
+      email,
+      dateFrom,
+      dateTo,
+      area,
+      companyName,
+      textArea,
+    });
+    res.status(200).json(mod);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
+// update mod
 const updateMod = async (req, res) => {
-  res.status(200).json({ mssg: "update mod application" });
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ error: "no such workout" });
+  }
+
+  const mod = await Mod.findOneAndUpdate({ _id: id }, { ...req.body });
+
+  if (!mod) {
+    return res.status(400).json({ error: "No such workout" });
+  }
+
+  res.status(200).json(mod);
 };
 
-export { createMod, updateMod };
+export { getMod, getMods, createMod, updateMod };
--- server/routes/modRoutes.js
+++ server/routes/modRoutes.js
@@ -1,12 +1,23 @@
 import express from "express";
-import { createMod, updateMod } from "../controllers/modController.js";
+import {
+  getMod,
+  getMods,
+  createMod,
+  updateMod,
+} from "../controllers/modController.js";
 
 const router = express.Router();
 
-// create mod
+// GET all mod
+router.get("/", getMods);
+
+// GET a single mod
+router.get("/:id", getMod);
+
+// POST a new mod
 router.post("/", createMod);
 
-// update
-router.put("/:id", updateMod);
+// UPDATE a mod
+router.patch("/:id", updateMod);
 
 export default router;
